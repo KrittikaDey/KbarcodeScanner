a27cf0b50251f5b60329a8d486f1335cfb5c257e
Krittika <krittika.bluehorse@gmail.com>
2017-04-04 12:00:29 +0530

    BarcodeScanner
    Simple barcode scanner for scan barcode and get barcode type

---
+++ Kbarcodescanner/app/src/main/java/com/example/krittika/kbarcodescanner/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.krittika.kbarcodescanner;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mButton;
+    private TextView textView,textView2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView)findViewById(R.id.textView);
+        textView2 = (TextView)findViewById(R.id.textView2);
+
+        // Scanner
+
+        mButton = (Button) findViewById(R.id.button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
+                integrator.initiateScan();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (scanResult != null) {
+            String re = scanResult.getContents();
+            String scanFormat = scanResult.getFormatName();
+           // Log.d("code", re);
+            Toast.makeText(getApplicationContext(),re,Toast.LENGTH_SHORT).show();
+            textView.setText("CODE: " +re);
+            textView2.setText("FORMAT: " + scanFormat);
+        }
+        // else continue with any other code you need in the method
+
+
+    }
+}
